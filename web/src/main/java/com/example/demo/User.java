18f55dc5cc9130b645a13fd510349b0d7e1dd020
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-04-12 22:03:55 +0530

    completed review comments

---
+++ web/src/main/java/com/example/demo/User.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class User{
+	private String userName;
+	private int userId;
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	}
